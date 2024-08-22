0aa6398ff3041a6b2d7a8030bdebea5b30132b12
Wmlster <maniksharma2630@gmail.com>
2024-08-22 12:26:13 +0530

    kl

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const createIssue=document.getElementById("create-issue");
 const issueInput=document.getElementById("issue-input");
+const todoContainer=document.getElementById("todo");
 
 createIssue.addEventListener("click", onCreateClick);
+
+issueInput.addEventListener("blur", onBlurCreateIssueInput);
+issueInput.addEventListener("keyup", onEnterInput);
+
 
 function toggleCreateIssueOptions(){
     createIssue.classList.toggle("hide");
@@ -13,9 +18,42 @@ function toggleCreateIssueOptions(){
 }
 
 function onBlurCreateIssueInput(){
-    toggleCreateIssueOptions();
+
+    if(!issueInput.classList.contains("hide")){
+        toggleCreateIssueOptions();
+    }
 }
 
 function onCreateClick(){
     toggleCreateIssueOptions();
+}
+
+function onEnterInput(e){
+    if(e.keyCode===13){
+        const issueName=issueInput.value;
+
+        if(!issueName){
+            return;
+        }
+
+        const issueCard=document.createElement("div");
+        issueCard.className="card";
+
+        issueCard.innerHTML=`
+        <span>${issueName}</span>
+        <span class="material-icons" onclick="deleteCard(this)">delete</span>
+        `;
+
+        issueInput.value="";
+        todoContainer.appendChild(issueCard);
+        toggleCreateIssueOptions();
+        //issueInput.blur();
+
+    }
+
+}
+
+function deleteCard(deleteButton){
+    const card=deleteButton.parentNode;
+    card.remove();
 }
